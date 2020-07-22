45a4f6da75695c000069472c699905f597ca3b5d
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-07-22 13:55:47 -0700

    add controllers and html
    Co-authored-by: JohnNils Olson <johnnils@gmail.com>

---
+++ Packer/Controllers/PackerController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Packer.Models;
+
+namespace Packer.Controllers
+{
+  public class PackerControllers : Controllers
+  {
+    [HttpGet("/packer")]
+    public ActionResult Index()
+    {
+      List<Packer> allPackers = Packer.GetAll();
+      return View(allPackers);
+    }
+    [HttpGet("/packer/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/packer")]
+    public ActionResult New(string tripName)
+    {
+      Packer newPacker = new Packer(tripName);
+      return RedirectToAction("Index");
+    }
+    [HttpGet("/packer/{Id}")]
+    public ActionResult Show(int Id)
+    {
+      Packer foundPacker = Packer.Find(Id);
+      return View(foundPacker);
+    }
+    [HttpPost("/packer/delete")]
+    public ActionResult DeleteAll()
+    {
+      Packer.ClearAll();
+      return View();
+    }
+  }
+}
