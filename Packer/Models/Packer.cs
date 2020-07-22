a7a2937436878705cdb95971d75635f7d01e3919
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-07-22 13:34:23 -0700

    add constructor and declarations
    Co-authored-by: JohnNils Olson <johnnils@gmail.com>

--- Packer/Models/Packer.cs
+++ Packer/Models/Packer.cs
@@ -4,7 +4,17 @@ namespace Packer.Models
 {
   public class Packer
   {
-    public string tripName { get; set; }
-    
+    public string TripName { get; set; }
+    public int Id { get; }
+    public Dictionary<string, string> Gear { get; set; }
+    private static List<Packer> _instances = new List<Packer> {};
+    public Packer(string tripName)
+    {
+      TripName = tripName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Dictionary<string, string> gear = new Dictionary<string, string> {};
+
+    }
   }
 }
